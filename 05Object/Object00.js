4702edce993714889b9edf3736ac108360c2011a
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-30 20:40:08 +0700

    Object trong JavaScript

---
+++ 05Object/Object00.js
@@ -0,0 +1,21 @@
+/**var a = {
+ *  key1: value1,
+ *  key2: value2
+ * }; */
+let myDog = {
+    name: 'Tút',
+    weight: 16,
+    isAlie: false
+};
+console.log(myDog.name);
+console.log(myDog['weight']);
+let a = {}; // camelCase
+/**Quy chuẩn đặt tên trong lập trình:
+ * Trong lập trình quy chuẩn đầu tiên cần đưa ra là quy chuẩn cho việc đặt tên.
+ * Có hàng tỉ thứ cần đặt tên trong lập trình, nói chơi vậy thôi chứ phân loại ra khoảng hơn chục thôi à,
+ * ví dụ như tên Class, tên biến, tên phương thức, tên thuộc tính...
+ * Có 3 chuẩn để đặt tên là underscore, camelCase và PascalCase.
+ * - underscore: sử dụng dấu gạch chân giữa các từ, tất cả các từ đều viết thường, ví dụ: $this_is_my_variable.
+ * - camelCase: giống như cách viết của nó, từ đầu tiên viết thường, các từ tiếp theo viết hoa chữ cái đầu, ví dụ $thisIsMyVariable.
+ * - PascalCase: viết hoa tất cả các chữ cái đầu, ví dụ $ThisIsMyVariable.
+ */
---
+++ 05Object/Object01.js
@@ -0,0 +1,24 @@
+/**
+ * Sử dụng kiến thức đã học khai báo vài object khác nhau mà bạn có thể tưởng tượng ra
+ */
+var myCommunity = {
+    name: 'Coders.Tokyo',
+    numberOfMember: 10000,
+    isAwesome: true
+};
+
+let myCat = {
+    name: 'Sứt',
+    age: 1.3,
+    isAlie: true,
+    isMale: null
+};
+
+let myHouse = {
+    area: 110,
+    nFloor: 5
+};
+
+console.log(myCommunity['name']);
+console.log(myCat);
+console.log(myHouse.nFloor);
