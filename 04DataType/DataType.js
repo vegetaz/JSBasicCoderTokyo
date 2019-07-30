0c0f36d64daba961c71e6aa5f01de3e08aef1500
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-30 20:18:11 +0700

    Kiểu dữ liệu (Phần 1)

---
+++ 04DataType/DataType.js
@@ -0,0 +1,13 @@
+/**Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
+ * Ví dụ dưới đây mô tả thời tiết ngày hôm nay:
+ * var today = 'Sunday';
+ * var isCloudy = true;
+ * var currentTemperature = 30;
+ */
+let name = 'Linh';
+let age = 26;
+let isHuman = true;
+let isMale = false;
+let ngao;
+ngao = null;
+console.log(name, age, isHuman, isMale, ngao);
