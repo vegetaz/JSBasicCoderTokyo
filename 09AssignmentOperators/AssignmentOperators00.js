1427aa934f6cf47859a4524b86f0ace73c9bee1f
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-30 23:09:53 +0700

    Giới thiệu về các phép gán trong JavaScript

---
+++ 09AssignmentOperators/AssignmentOperators00.js
@@ -0,0 +1,20 @@
+/**
+ * Assignment Operators
+ * Các phép gán
+ * 4. = += -= *= /=
+ */
+let a;
+let b;
+a = b = 1;
+console.log(b);
+
+a += 5;
+console.log(a);
+
+let c = 2;
+c -= 5;
+console.log(c);
+
+let d = 8;
+d *= 2;
+console.log('*=',d);
---
+++ 09AssignmentOperators/AssignmentOperators01.js
@@ -0,0 +1,7 @@
+let a = 10;
+/**
+ * Viết ra tất cả các cách khác nhau có thể (trong phạm vi bài học) để tăng a lên 2 đơn vị
+ */
+a += 2; // a = a + 2;
+
+console.log(a);
