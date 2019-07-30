2ac567c74a13e7c6f077ebcdaf1997aff5b7e42d
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-30 20:12:38 +0700

    Biến trong JavaScript

---
+++ 03Variables/Variable.js
@@ -0,0 +1,16 @@
+/**Biến trong JavaScript
+ * Khai báo 2 biến x, y nhận giá trị bất kỳ
+ * Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến
+ */
+let x = 25;
+let y = 89;
+let s = x + y;
+let h = x - y;
+let k = x * y;
+let l = x/y;
+console.log(x);
+console.log(y);
+console.log(s);
+console.log(h);
+console.log(k);
+console.log(l);
