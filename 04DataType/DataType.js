a89e36f555a9c8195a48f4e4947c1416081e5cc3
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-30 20:25:25 +0700

    Kiểu dữ liệu (Phần 1)

--- 04DataType/DataType.js
+++ 04DataType/DataType.js
@@ -1,3 +1,25 @@
+/**Data types:
+ * Primitive types:
+ *  - Number
+ *  - String
+ *  - Boolean
+ * Special types:
+ *  - undefined
+ *  - null
+ * Reference types:
+ *  - Array
+ *  - Object
+ */
+var a1 = -25;
+var a2 = 8.9;
+var str = 'Hello World!';
+var str2 = "Xin Chào!";
+var str3 = 'Xin Chào! I\'m Lem';
+var isHidden = false;
+var isCat = true;
+var a; // undefined
+a = 25101989;
+a = null;
 /**Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
  * Ví dụ dưới đây mô tả thời tiết ngày hôm nay:
  * var today = 'Sunday';
