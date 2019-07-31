874592e0c6931bc6280041f0dd0e1760e165a541
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-07-31 22:19:36 +0700

    Các loại vòng lặp khác với cú pháp đơn giản, dễ sử dụng

---
+++ 14ForOfForIn/ForOfForIn07.js
@@ -0,0 +1,18 @@
+let ngao = [
+    {
+        name: 'Linh',
+        age: 26
+    },
+    {
+        name: 'Sứt',
+        age: 1.3
+    },
+    {
+        name: 'Lem',
+        age: 0.4
+    }
+];
+
+for (let c of ngao) {
+    console.log(c.name, c.age);
+}
