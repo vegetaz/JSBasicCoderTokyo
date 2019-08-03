f23c93629968056975113d7335276caa4b64cee9
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-08-03 16:15:39 +0700

    Hướng dẫn cài đặt NodeJS trên Windows và Sublime

---
+++ 27NodeJSSublimeTextWindows/NodeJSSublimeTextWindows00.js
@@ -0,0 +1,18 @@
+/**
+ * Hướng dẫn cài đặt NodeJS trên Windows và Sublime
+ * https://www.youtube.com/watch?v=XNBA8dXllJc
+ * NodeJS
+ * https://nodejs.org/en/
+ * Babun - a windows shell you will love [PROJECT DISCONTINUED]
+ * https://babun.github.io/
+ * Portable console emulator for Windows
+ * https://cmder.net/
+ */
+
+ function Mouse(name) {
+     this.name = name;
+ }
+
+ let mickey = new Mouse('Mickey');
+
+ console.log(mickey);
