13cab9b101ffc1f168119374c3b9cde64fc77ede
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-08-03 16:27:29 +0700

    Hướng dẫn cài đặt NodeJS trên Mac

---
+++ 28NodeJSSublimeTextMac/NodeJSSublimeTextMac00.js
@@ -0,0 +1,10 @@
+/**
+ * Hướng dẫn cài đặt NodeJS trên Mac
+ * https://www.youtube.com/watch?v=0WhMcMMjUuE
+ */
+
+ console.log('Coder Tokyo');
+
+ console.log('Hướng dẫn cài đặt NodeJS trên Mac');
+
+ console.log('https://www.youtube.com/watch?v=0WhMcMMjUuE');
